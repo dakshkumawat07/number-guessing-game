1698fd1c1c6c243620ede0b963e6e719930fc454
Daksh Kumawat <dakshkumawat239@gmail.com>
2026-07-14 03:54:18 -0400

    Add difficulty selection with dynamic number range

--- main.c
+++ main.c
@@ -6,15 +6,45 @@ int main()
 {
     srand(time(NULL));
 
-    int secretNumber = (rand() % 100) + 1;
+    int secretNumber;
     int guess = 0;
     int attempts = 0;
+    int difficulty;
+    int maxNumber;
 
-    printf("=================================\n");
-    printf("   Number Guessing Game 🎮\n");
-    printf("=================================\n");
-    printf("Guess a number between 1 and 100.\n\n");
+     printf("=================================\n");
+     printf("   Number Guessing Game 🎮\n");
+     printf("=================================\n\n");
 
+     printf("Choose Difficulty\n\n");
+     printf("1. Easy (1 - 50)\n");
+     printf("2. Medium (1 - 100)\n");
+     printf("3. Hard (1 - 500)\n\n");
+
+     printf("Enter your choice: ");
+     scanf("%d", &difficulty);
+     switch (difficulty)
+{
+    case 1:
+        maxNumber = 50;
+        break;
+
+    case 2:
+        maxNumber = 100;
+        break;
+
+    case 3:
+        maxNumber = 500;
+        break;
+
+    default:
+        printf("\nInvalid choice!\n");
+        return 1;
+}
+     secretNumber = (rand() % maxNumber) + 1;
+
+     printf("\nGuess a number between 1 and %d.\n\n",maxNumber);
+;
     while (guess != secretNumber)
     {
         printf("Enter your guess: ");
